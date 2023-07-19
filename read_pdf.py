e892f518427d40d0d4bf800bad29f7a96c915b9f
clhnn <139110441+clhnn@users.noreply.github.com>
2023-07-19 16:26:36 +0800

    Add files via upload

---
+++ read_pdf.py
@@ -0,0 +1,20 @@
+from PDF import PDFProcessor
+if __name__ == '__main__':
+    
+    # 請將 "your file name.pdf" 替換為要處理的實際 PDF 檔案名稱
+    pdf_processor = PDFProcessor("222.pdf")
+
+    #從PDF中提取圖片
+    pdf_processor.extract_images()
+
+    # 從 PDF 中提取表格並儲存為單獨的 Excel 檔案
+    pdf_processor.extract_tables()
+
+    # 建立包含頁碼和內容的 PDF 簡單大綱
+    pdf_processor.create_simple_outline()
+
+    # 處理 PDF，提取圖片、表格並建立大綱
+    pdf_processor.process_pdf()
+
+    # 處理 PDF 並將結果匯出成 JSON 檔案
+    pdf_processor.process_and_export_json()

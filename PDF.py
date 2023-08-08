5668702f527cc87a5359154099579aba48405c1b
Jennyyun1017 <139110612+Jennyyun1017@users.noreply.github.com>
2023-08-08 16:17:48 +0800

    Update PDF.py

--- PDF.py
+++ PDF.py
@@ -2,13 +2,16 @@ import json
 import pandas as pd
 import pdfplumber
 import os
+import fitz
 from PyPDF2 import PdfReader
+import PyPDF2
 
 class PDFProcessor:
     
     # 初始化方法，設定要處理的 PDF 檔案名稱    
     def __init__(self, pdf_file):
         self.pdf_file = pdf_file
+        self.font_size_threshold = 12     #字體大小閾值，可根據需要進行調整
         
     # 讀取 PDF 的圖片並儲存成檔案   
     def extract_images(self):
@@ -21,6 +24,7 @@ class PDFProcessor:
                     
     # 讀取 PDF 的表格並儲存成獨立的 csv 檔案
     def extract_tables(self, odname=None):
+        odname = None
         pdf_path = self.pdf_file
         pdf = pdfplumber.open(pdf_path)
         already_taken = 'False'
@@ -29,7 +33,7 @@ class PDFProcessor:
         for pagenum, page in enumerate(pdf.pages):
             print('>>checking table at page %d'%(pagenum))
             tables = page.extract_tables()
-        
+
             if not tables:
                 print('>>skipped table at page %d'%(pagenum))
                 continue
@@ -42,9 +46,9 @@ class PDFProcessor:
                     continue
                 for t, table in enumerate(tables):
                     if odname != None:
-                        csv_name = os.path.join(odname, f'page{pagenum+1}_table{t+1}.csv',index=False)
+                        csv_name = os.path.join(odname, f'table{t+1}_{pagenum+1}.csv',index=False)
                     else:
-                        csv_name = f'page{pagenum+1}_table{t+1}.csv'
+                        csv_name = f'table{t+1}_{pagenum+1}.csv'
                     if already_taken == 'True':
                         if table == tables[0]:
                             continue
@@ -58,20 +62,10 @@ class PDFProcessor:
                         for page_table in range(1, count+1):
                             table += pdf.pages[pagenum+page_table].extract_tables()[0]
                         combined_table = pd.DataFrame(table[1:], columns = table[0])
-                        combined_table.rename(columns={None: '#'}, inplace=True)
-                        for index, row in combined_table.iterrows():
-                            for col in range(0, len(combined_table.columns)):
-                                if combined_table.iat[index, col] == None:
-                                    combined_table.iat[index, col] = '#'
                         combined_table.to_csv(csv_name,index=False)
                         already_taken = 'True'
                         continue
                     df_detail = pd.DataFrame(table[1:], columns = table[0])
-                    df_detail.rename(columns={None: '#'}, inplace=True)
-                    for index, row in df_detail.iterrows():
-                        for col in range(0, len(df_detail.columns)):
-                            if df_detail.iat[index, col] == None:
-                                df_detail.iat[index, col] = '#'
                     df_detail.to_csv(csv_name,index=False)
         
             else:
@@ -80,42 +74,63 @@ class PDFProcessor:
                     continue
                 for t2, table in enumerate(tables):
                     if odname != None:
-                        csv_name = os.path.join(odname, f'page{pagenum+1}_table{t2+1}.csv',index=False)
+                        csv_name = os.path.join(odname, f'table{t2+1}_{pagenum+1}.csv',index=False)
                     else:
-                        csv_name = f'page{pagenum+1}_table{t2+1}.csv'
+                        csv_name = f'table{t2+1}_{pagenum+1}.csv'
                     if already_taken == 'True':
                         if table == tables[0]:
                             already_taken = 'False'
                             continue
                     df_detail = pd.DataFrame(table[1:], columns = table[0])
-                    df_detail.rename(columns={None: '#'}, inplace=True)
-                    for index, row in df_detail.iterrows():
-                        for col in range(0, len(df_detail.columns)):
-                            if df_detail.iat[index, col] == None:
-                                df_detail.iat[index, col] = '#'
                     df_detail.to_csv(csv_name,index=False)
                     already_taken = 'False'
         return
+    
+    # 讀取 PDF 頁首頁碼內文
+    def classify_text_by_font_size(self):
+        page_texts = {'header': [], 'content': []}
+        pdf_document = fitz.open(self.pdf_file)
+        for page_num in range(pdf_document.page_count):
+            page = pdf_document[page_num]
+            text = page.get_text()
+            header_text = ''
+            content_text = ''
+            for line in text.split('\n'):
+                if line.strip():
+                    font_size = None
+                    for block in page.get_text("dict")['blocks']:
+                        for line_info in block['lines']:
+                            for span in line_info['spans']:
+                                if line in span['text']:
+                                    font_size = span['size']
+                                    break
+                            if font_size is not None:
+                                break
+                        if font_size is not None:
+                            break
+    
+                    if font_size is not None and font_size < self.font_size_threshold:
+                        header_text += line + '\n'
+                    else:
+                        content_text += line + '\n'
+    
+            # Store classified text in dictionary
+            page_texts['header'].append(header_text)
+            content_all_text = self.extract_paragraphs(content_text, page_num)  # Fix: remove the extra argument
+            page_texts['content'].append(content_all_text)
+        return page_texts
 
     # 讀取 PDF 的每頁的段落
-    def extract_all_paragraphs(self):
-        pdf_file = open(self.pdf_file, 'rb')
-        pdf = pdfplumber.open(pdf_file)
-        pdf_reader = PdfReader(pdf_file) 
-        extract_all_paragraphs = []
-        for page_num in range(len(pdf_reader.pages)):
-            extract_paragraphs = []
-            page = pdf_reader.pages[page_num]
-            page_text = page.extract_text()
-            page_text = page_text.strip('\n')
-            paragraphs = page_text.split(' \n')  # Split on multiple line breaks
-            while '' in paragraphs:
-                paragraphs.remove('')
-            for i, paragraph in enumerate(paragraphs):
-                paragraph_number = f"Page {page_num + 1}, Paragraph {i + 1}:{paragraph}"
-                extract_paragraphs.append(paragraph_number)
-            extract_all_paragraphs.append(extract_paragraphs)
-        return extract_all_paragraphs
+    def extract_paragraphs(self,text,page_num):
+        paragraphs = text.split(' \n') 
+        all_para=[]
+        while '' in paragraphs:
+            paragraphs.remove('')
+        for i, paragraph in enumerate(paragraphs):
+            paragraph_number = f"Page {page_num + 1}, Paragraph {i + 1}:{paragraph}\n"
+            #print(paragraph_number)
+            all_para.append(paragraph_number)
+        return all_para
                                 
     # 建立 PDF 的簡單大綱，包含頁碼和內容
     def create_simple_outline(self):
@@ -125,16 +140,28 @@ class PDFProcessor:
             outline = {
                 'children': []
             }
-            content_all = self.extract_all_paragraphs()  #page.extract_text()
+            content_all=self.classify_text_by_font_size()
             for page_num in range(num_pages):
-                page = pdf_reader.pages[page_num]
-                content = content_all[page_num]
-                page_node = {
-                    'title': f'Page {page_num + 1}',
-                    'page': page_num + 1,
-                    'content': content
-                }
-                outline['children'].append(page_node)
+                for category, texts in content_all.items():
+                    content = texts[page_num]
+                    if category == 'header':
+                        page_node = {
+                            'title': f'Page {page_num + 1}',
+                            'page': page_num + 1,
+                            'category': category,
+                            'header': content,
+                            
+                        }
+                        outline['children'].append(page_node)
+                    else:
+                        page_node = {
+                            'title': f'Page {page_num + 1}',
+                            'page': page_num + 1,
+                            'category': category,
+                            'content': content,
+                            
+                        }
+                        outline['children'].append(page_node)
         return outline
         
     # 處理 PDF，提取圖片、表格並建立大綱
@@ -160,7 +187,7 @@ class PDFProcessor:
 if __name__ == '__main__':
     
     # 請將 "your file name.pdf" 替換為要處理的實際 PDF 檔案名稱
-    pdf_processor = PDFProcessor("your file name.pdf")
+    pdf_processor = PDFProcessor("demo2-5.pdf")
 
     #從PDF中提取圖片
     pdf_processor.extract_images()
